942470d25627005d143555958f14e43c0a86dad4
Miroslav Novta <miroslav.novta@gmail.com>
2024-02-04 22:02:40 +0100

    Added activities

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from librelingo_yaml_loader import yaml_loader
 from librelingo_json_export.export import export_course
 
 course = yaml_loader.load_course("./course") 
-export_course("./LibreLingo-RSK-from-SR", course)
+export_course("../../apps/web/src/courses/rusyn-from-serbian", course)
